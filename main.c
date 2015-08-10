5e8b319c0f6484727c5e4f9bee03a3362b2301e5
Toby Alden <toby.alden@gmail.com>
2015-08-10 16:28:26 -0700

    Break hex string into hex characters.

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <gmp.h>
 
 int main(int argc, char *argv[])
 {
-  printf("HEX INPUT: %s\n", argv[1]);
+  char *hex = argv[1];
+  printf("HEX INPUT: %s\n", hex);
+
+  char hex_char[3];
+  hex_char[2] = '\0';
+  for(int i = 0; hex[i] != '\0'; i++)
+  {
+    hex_char[i%2] = hex[i];
+    if(i%2 != 0)
+    {
+      printf("%s\n", &hex_char[0]);
+    }
+  }
+  printf("\n");
 }
