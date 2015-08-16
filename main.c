a07eb06c5aae86958c3f4a8fa26b6b3c67a72a74
Toby Alden <toby.alden@gmail.com>
2015-08-15 21:34:50 -0700

    Added letter pair frequency scoring. Correct decryption now has highest score.

--- main.c
+++ main.c
@@ -4,9 +4,11 @@
 #include <string.h>
 
 const char HEX_CHARS[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+const double PAIR_WEIGHT = 0.0384848485;
 
 char * fixed_xor(char *hex, char *mask);
 double freq_score(char c);
+double pair_freq_score(char a, char b);
 
 int main(int argc, char *argv[])
 {
@@ -24,7 +26,7 @@ int main(int argc, char *argv[])
   char *decrypted_msg;
   for(int i = 0; i < 256; i++)
   {
-    char decrypt_attempt[hex_length/2];
+    char decrypt_attempt[hex_length/2+1];
     xor[0] = HEX_CHARS[i/16];
     xor[1] = HEX_CHARS[i%16];
     printf("%s: ", xor);
@@ -34,6 +36,7 @@ int main(int argc, char *argv[])
     char hex_char[3];
     hex_char[2] = '\0';
     double decrypt_score = 0;
+    char prev_letter;
     for(int j = 0; hex[j] != '\0'; j++)
     {
       hex_char[j%2] = hex[j];
@@ -43,13 +46,20 @@ int main(int argc, char *argv[])
         char letter = (char) strtol(xored_char, NULL, 16);
         decrypt_attempt[(j-1)/2] = letter;
         decrypt_score += freq_score(letter);
+        if(j != 1)
+        {
+          decrypt_score += pair_freq_score(prev_letter, letter) * PAIR_WEIGHT;
+        }
+        prev_letter = letter;
       }
     }
     if(decrypt_score > high_score)
     {
       high_score = decrypt_score;
-      decrypted_msg = decrypt_attempt;
+      strcpy(decrypted_msg, decrypt_attempt);
+      // decrypted_msg = decrypt_attempt;
     }
+    decrypt_attempt[hex_length/2] = '\0';
     printf("%s (Score: %f)\n", decrypt_attempt, decrypt_score);
   }
   printf("Decrypted message: %s (High Score: %f)\n", decrypted_msg, high_score);
@@ -113,6 +123,63 @@ double freq_score(char c)
     default:
       return 0;
   }
+}
+
+double pair_freq_score(char a, char b)
+{
+  if(a == 't' && b == 'h') { return 330; }
+  if(a == 'h' && b == 'e') { return 302; }
+  if(a == 'a' && b == 'n') { return 181; }
+  if(a == 'e' && b == 'r') { return 169; }
+  if(a == 'n' && b == 'd') { return 146; }
+  if(a == 'r' && b == 'e') { return 133; }
+  if(a == 'e' && b == 'd') { return 126; }
+  if(a == 'e' && b == 's') { return 115; }
+  if(a == 'o' && b == 'u') { return 115; }
+  if(a == 't' && b == 'o') { return 115; }
+  if(a == 'h' && b == 'a') { return 114; }
+  if(a == 'e' && b == 'n') { return 111; }
+  if(a == 'e' && b == 'a') { return 110; }
+  if(a == 's' && b == 't') { return 109; }
+  if(a == 'n' && b == 't') { return 106; }
+  if(a == 'o' && b == 'n') { return 106; }
+  if(a == 'a' && b == 't') { return 104; }
+  if(a == 'h' && b == 'i') { return 97; }
+  if(a == 'a' && b == 's') { return 95; }
+  if(a == 'i' && b == 't') { return 93; }
+  if(a == 'n' && b == 'g') { return 92; }
+  if(a == 'i' && b == 's') { return 86; }
+  if(a == 'o' && b == 'r') { return 84; }
+  if(a == 'e' && b == 't') { return 83; }
+  if(a == 'o' && b == 'f') { return 80; }
+  if(a == 't' && b == 'i') { return 76; }
+  if(a == 'a' && b == 'r') { return 75; }
+  if(a == 't' && b == 'e') { return 75; }
+  if(a == 's' && b == 'e') { return 74; }
+  if(a == 'm' && b == 'e') { return 68; }
+  if(a == 's' && b == 'a') { return 67; }
+  if(a == 'n' && b == 'e') { return 66; }
+  if(a == 'w' && b == 'a') { return 66; }
+  if(a == 'v' && b == 'e') { return 65; }
+  if(a == 'l' && b == 'e') { return 64; }
+  if(a == 'n' && b == 'o') { return 60; }
+  if(a == 't' && b == 'a') { return 59; }
+  if(a == 'a' && b == 'l') { return 57; }
+  if(a == 'd' && b == 'e') { return 57; }
+  if(a == 'o' && b == 't') { return 57; }
+  if(a == 's' && b == 'o') { return 57; }
+  if(a == 'd' && b == 't') { return 56; }
+  if(a == 'l' && b == 'l') { return 56; }
+  if(a == 't' && b == 't') { return 56; }
+  if(a == 'e' && b == 'l') { return 55; }
+  if(a == 'r' && b == 'o') { return 55; }
+  if(a == 'a' && b == 'd') { return 52; }
+  if(a == 'd' && b == 'i') { return 50; }
+  if(a == 'e' && b == 'w') { return 50; }
+  if(a == 'r' && b == 'a') { return 50; }
+  if(a == 'r' && b == 'i') { return 50; }
+  if(a == 's' && b == 'h') { return 50; }
+  else { return 0; }
 }
 
 char * fixed_xor(char *hex, char *mask)
